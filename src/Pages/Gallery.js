8ffe5e8f31d689317379cec910bc281dab0e5f33
iddev5 <ayushbardhan5@gmail.com>
2025-01-14 11:27:18 +0530

    Add competition photos to gallery

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -54,12 +54,61 @@ import dp7 from '../Assets/gallery/dp24/24DP7.jpg';
 import dp8 from '../Assets/gallery/dp24/24DP8.jpg';
 import dp9 from '../Assets/gallery/dp24/24DP9.jpg';
 
+import comp1 from '../Assets/gallery/comp/comp1.jpg';
+import comp2 from '../Assets/gallery/comp/comp2.jpg';
+import comp3 from '../Assets/gallery/comp/comp3.jpg';
+import comp4 from '../Assets/gallery/comp/comp4.jpg';
+import comp5 from '../Assets/gallery/comp/comp5.jpg';
+import comp6 from '../Assets/gallery/comp/comp6.jpg';
+import comp7 from '../Assets/gallery/comp/comp7.jpg';
+import comp8 from '../Assets/gallery/comp/comp8.jpg';
+import comp9 from '../Assets/gallery/comp/comp9.jpg';
+import comp10 from '../Assets/gallery/comp/comp10.jpg';
+import comp11 from '../Assets/gallery/comp/comp11.jpg';
+import comp12 from '../Assets/gallery/comp/comp12.jpg';
+import comp13 from '../Assets/gallery/comp/comp13.jpg';
+import comp14 from '../Assets/gallery/comp/comp14.jpg';
+import comp15 from '../Assets/gallery/comp/comp15.jpg';
+import comp16 from '../Assets/gallery/comp/comp16.jpg';
+import comp17 from '../Assets/gallery/comp/comp17.jpg';
+import comp18 from '../Assets/gallery/comp/comp18.jpg';
+import comp19 from '../Assets/gallery/comp/comp19.jpg';
+import comp20 from '../Assets/gallery/comp/comp20.jpg';
+
 function Gallery() {
  const[Sidebar,SetSidebar]=useState(true) 
   return (
     <>
       <Header Sidebar={Sidebar} SetSidebar={SetSidebar} />
+
       <div className="center">
+      
+        <div className="gallery-header">
+          <h1>Competition on 22.09.24 on Moral storytelling and Drawing</h1>
+        </div>
+        <div className="gallery">
+          <img src={comp1} />
+          <img src={comp2} />
+          <img src={comp3} />
+          <img src={comp4} />
+          <img src={comp5} />
+          <img src={comp6} />
+          <img src={comp7} />
+          <img src={comp8} />
+          <img src={comp9} />
+          <img src={comp10} />
+          <img src={comp11} />
+          <img src={comp12} />
+          <img src={comp13} />
+          <img src={comp14} />
+          <img src={comp15} />
+          <img src={comp16} />
+          <img src={comp17} />
+          <img src={comp18} />
+          <img src={comp19} />
+          <img src={comp20} />
+        </div>
+
         <div className="gallery-header">
           <h1>Tithi Puja Celebration of Holy Mother Sri Sarada Devi</h1>
         </div>
